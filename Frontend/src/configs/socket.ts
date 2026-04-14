6bcb121dd528f073eae411015c02d9c8894da99b
TusharBhardwaj47 <tusharbhardwaj635@gmail.com>
2026-04-15 04:07:44 +0530

    updated backend URL for production

--- Frontend/src/configs/socket.ts
+++ Frontend/src/configs/socket.ts
@@ -3,12 +3,10 @@ import { io } from "socket.io-client";
 const socket = io("https://online-code-collaborator-2.onrender.com", {
   autoConnect: false,
   transports: ["websocket"],
-})
-
   auth: {
     token: localStorage.getItem("token"),
-    name; "Tushar"
+    name: "Tushar"
   }
-});
+})
 
 export default socket;
